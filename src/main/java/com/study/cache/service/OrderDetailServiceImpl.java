140a3353a73a6bfb937b59a7d03b26c2f944c2c2
guowang <sysiongw@sysion.com>
2017-12-13 19:01:54 +0800

    更新

--- src/main/java/com/study/cache/service/OrderDetailServiceImpl.java
+++ src/main/java/com/study/cache/service/OrderDetailServiceImpl.java
@@ -11,12 +11,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
-import com.dongnao.cache.lock.DistributedLock;
 import com.google.common.base.Charsets;
 import com.google.common.hash.BloomFilter;
 import com.google.common.hash.Funnels;
 import com.study.cache.dao.TOrderDetailDao;
 import com.study.cache.entity.TOrderDetail;
+import com.study.cache.lock.DistributedLock;
 
 
 @Service
@@ -95,36 +95,37 @@ public class OrderDetailServiceImpl implements OrderDetailService {
 		cs.cachePut(userCode, orderAmount, CACHE_NAME);
 		return orderAmount;
 		
-//		if(lock.getLock()){
-//			logger.info("======================get data from db============================");
-//			BigDecimal orderAmount = mapper.getOrderAmount(userCode);
-//			cs.cachePut(userCode, orderAmount, CACHE_NAME);
-//			cs.cachePut(userCode+"_backup", orderAmount, CACHE_NAME_BACKUP);
-//			lock.releaseLock();
-//			return orderAmount;
-//		}else{
-//			logger.info("======================get data from backup cache============================");
-//			return cs.cacheResult(userCode+"_backup", CACHE_NAME_BACKUP);
-//		}
+		/*if(lock.getLock()){
+			logger.info("======================get data from db============================");
+			BigDecimal orderAmount = mapper.getOrderAmount(userCode);
+			cs.cachePut(userCode, orderAmount, CACHE_NAME);
+			cs.cachePut(userCode+"_backup", orderAmount, CACHE_NAME_BACKUP);
+			lock.releaseLock();
+			return orderAmount;
+		}else{
+			logger.info("======================get data from backup cache============================");
+			return cs.cacheResult(userCode+"_backup", CACHE_NAME_BACKUP);
+		}
 		
+		//读库
+		synchronized (logger) {
+			cacheResult = cs.cacheResult(userCode, CACHE_NAME);
+			if(cacheResult!=null){
+				logger.info("======================get data from cache============================");
+				return cacheResult;
+			}
+			logger.info("======================get data from db============================");
+			BigDecimal orderAmount = mapper.getOrderAmount(userCode);
+			cs.cachePut(userCode, orderAmount, CACHE_NAME);
+			return orderAmount;
+		}
 		
-		//如果缓存没有值，从数据库里面取
-//		synchronized (logger) {
-//			cacheResult = cs.cacheResult(userCode, CACHE_NAME);
-//			if(cacheResult!=null){
-//				logger.info("======================get data from cache============================");
-//				return cacheResult;
-//			}
-//			logger.info("======================get data from db============================");
-//			BigDecimal orderAmount = mapper.getOrderAmount(userCode);
-//			cs.cachePut(userCode, orderAmount, CACHE_NAME);
-//			return orderAmount;
-//		}
-		
-//		logger.info("======================get data from db============================");
-//		BigDecimal orderAmount = mapper.getOrderAmount(userCode);
-//		cs.cachePut(userCode, orderAmount, CACHE_NAME);
-//		return orderAmount;
+		logger.info("======================get data from db============================");
+		BigDecimal orderAmount = mapper.getOrderAmount(userCode);
+		cs.cachePut(userCode, orderAmount, CACHE_NAME);
+		return orderAmount;*/
 	}
 
+	
+	
 }
